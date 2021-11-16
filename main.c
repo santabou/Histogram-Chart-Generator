efeec4e01a65cfe5b6b80b2a5788b7cf78cef43d
santabou <90387186+santabou@users.noreply.github.com>
2021-11-16 15:38:46 +0700

    .

---
+++ main.c
@@ -0,0 +1,20 @@
+//
+// Created by Thitiwat Sornmanee on 16/11/2021 AD.
+//
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+int main()
+{
+    FILE* data = fopen("Data.csv", "r");
+    if (data == Null)
+    {
+        printf("File not found")
+        quit
+    }
+    else
+    {
+
+    }
+}
